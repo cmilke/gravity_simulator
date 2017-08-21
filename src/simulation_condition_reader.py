4f2eaab60b6e70f3e659706abce713e9447aca95
cmilke <chrisdmilke@gmail.com>
2017-08-21 18:03:58 -0500

    made global scale factors setable in dat file

--- src/simulation_condition_reader.py
+++ src/simulation_condition_reader.py
@@ -1,20 +1,21 @@
-_gravitational_constant = 6.674e-11 #alters the force of gravity. 6.67e-11 is the real-world value
-_distance_scale_factor = 1e-9 #makes everything bigger or smaller (basically the same as camera controls)
-_period_scale_factor = 5e-5 #changes how fast time moves. 1 = real-time, 1e-3 = 1000 times faster
-_velocity_scale_factor = _distance_scale_factor / _period_scale_factor
-_mass_scale_factor = _gravitational_constant * ( _distance_scale_factor**3 / _period_scale_factor**2 )
-
-
-
 def read(conditions_file_name, render_parameters):
     conditions_file = open(conditions_file_name,'r')
+
+    gravity_line = conditions_file.readline()
+    distance_line = conditions_file.readline()
+    period_line = conditions_file.readline()
+    gravitational_constant = float( gravity_line.split('#')[0] )
+    distance_scale_factor = float( distance_line.split('#')[0] )
+    period_scale_factor = float( period_line.split('#')[0] )
+    velocity_scale_factor = distance_scale_factor / period_scale_factor
+    mass_scale_factor = gravitational_constant * ( distance_scale_factor**3 / period_scale_factor**2 )
 
     mass_list = [] 
     init_pos = []
     init_vel = []
     colors = []
     markers = []
-    for line in conditions_file.readlines()[1:]:
+    for line in conditions_file.readlines()[2:]:
         split_line = line.split(';')
         mass_string = split_line[0]
         position_strings = split_line[1].split()
@@ -22,18 +23,18 @@ def read(conditions_file_name, render_parameters):
         color_string = split_line[3].strip()
         marker_string = split_line[4].strip()
 
-        mass = float(mass_string) * _mass_scale_factor
+        mass = float(mass_string) * mass_scale_factor
         mass_list.append(mass)
         colors.append(color_string)
         markers.append(marker_string)
 
-        pos_x = float(position_strings[0]) * _distance_scale_factor
-        pos_y = float(position_strings[1]) * _distance_scale_factor
-        pos_z = float(position_strings[2]) * _distance_scale_factor
+        pos_x = float(position_strings[0]) * distance_scale_factor
+        pos_y = float(position_strings[1]) * distance_scale_factor
+        pos_z = float(position_strings[2]) * distance_scale_factor
                                                                    
-        vel_x = float(velocity_strings[0]) * _velocity_scale_factor
-        vel_y = float(velocity_strings[1]) * _velocity_scale_factor
-        vel_z = float(velocity_strings[2]) * _velocity_scale_factor
+        vel_x = float(velocity_strings[0]) * velocity_scale_factor
+        vel_y = float(velocity_strings[1]) * velocity_scale_factor
+        vel_z = float(velocity_strings[2]) * velocity_scale_factor
 
         init_pos.append([pos_x,pos_y,pos_z])
         init_vel.append([vel_x,vel_y,vel_z])
